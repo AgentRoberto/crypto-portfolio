7459c756e4f97ab9b419e613ba3434fed5b2c2aa
Rob Sherwani <sherwanirobert@gmail.com>
2021-09-01 22:20:30 -0400

    button click event

--- crypto-portfolio/src/components/AddPair/AddPair.js
+++ crypto-portfolio/src/components/AddPair/AddPair.js
@@ -4,10 +4,12 @@ import './AddPair.scss';
 export const AddPair = (props) => {
     const [portfolio, setPortfolio] = useState([])
     
-   
+    const onAddBtnClick = () => {
+        setPortfolio(portfolio.concat(props.name));
+      };
     return (
         <div>
-            <button class="btn-add" >Add to Portfolio</button>
+            <button class="btn-add" onClick={onAddBtnClick}>Add to Portfolio</button>
             {portfolio}
         </div>
     )
