67daae3693155ba49ad2f40ae9584ef43571fb3f
Rob Sherwani <sherwanirobert@gmail.com>
2021-09-08 20:23:49 -0400

    Style changes and Navbar creation

--- crypto-portfolio/src/components/DashBoard/Dashboard.js
+++ crypto-portfolio/src/components/DashBoard/Dashboard.js
@@ -11,11 +11,11 @@ function Dashboard({ price, data }) {
     maintainAspectRatio: false
   };
   if (price === "0.00") {
-    return <h2>please select a currency pair</h2>;
+    return <h1>Please select a currency pair</h1>;
   }
   return (
     <div className="dashboard">
-      <h2>{`$${price}`}</h2>
+      <h2>{`Price = $${price}`}</h2>
 
       <div className="chart-container">
         <Line type="radar" data={data} options={opts} />
---
+++ crypto-portfolio/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.scss';
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className='crypto-wallet'>
+                <h1>Wallet Tracker</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
